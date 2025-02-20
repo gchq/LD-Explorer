fa72dec074a65fdd0821ebf7bb234ec49bf28ac2
GCHQDeveloper81 <175597767+GCHQDeveloper81@users.noreply.github.com>
2025-02-20 10:24:11 +0000

    Update catalog and dependencies (#79)

--- src/lib/stores/sources/remote-sources.store.ts
+++ src/lib/stores/sources/remote-sources.store.ts
@@ -21,5 +21,11 @@ export const sources = {
 
 export const sourceList = derived(
 	sources,
+	// TODO: Comunica tries to auto-detect the nature and querying capabilities of remote sources by
+	// sending off exploratory requests to endpoints etc.
+	//
+	// To improve performance,  we could expose "remote source type"  as an option to users rather
+	// than always defaulting to auto detect. In our sources catalog, for example, we know the
+	// DBPedia SPARQL endpoint is and will always be a SPARQL endpoint.
 	($sources) => $sources.filter((s) => s.enabled).map((s) => s.url) as QuerySources
 );
