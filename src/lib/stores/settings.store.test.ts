f57bc9d315f9411a636baf1abd5e3fa968fb62a0
Sylvester Kaczmarek <16242628+sylvesterkaczmarek@users.noreply.github.com>
2026-09-14 02:42:06 -0400

    Expose unionDefaultGraph setting (#266)

--- src/lib/stores/settings.store.test.ts
+++ src/lib/stores/settings.store.test.ts
@@ -6,5 +6,6 @@ import { settings } from './settings.store';
 describe('settings', () => {
 	it('has some sensible defaults', () => {
 		expect(Object.keys(get(settings))).not.toHaveLength(0);
+		expect(get(settings).general__unionDefaultGraph).toBe(true);
 	});
 });
